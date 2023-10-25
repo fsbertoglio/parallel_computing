7e82456f1592b9e0aa728d06705bd5d2b346e49d
Felipe Bertoglio <felipe.bertoglio@gmail.com>
2023-10-25 20:23:06 +0000

    update: resolved until 1.3

--- src/introduction/addVectors/addVectors.c
+++ src/introduction/addVectors/addVectors.c
@@ -2,34 +2,46 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define N 8
+#define N 20
 
-void initVector(int *A, int *B, int *C){
-    for(int i = 0; i < N; i++){
+void initVector(int *A, int *B, int *C)
+{
+    for (int i = 0; i < N; i++)
+    {
         A[i] = i;
         B[i] = N - i;
         C[i] = 0;
     }
 }
 
-void addVector(int *A, int *B, int *C){
-    #pragma omp parallel
+void addVector(int *A, int *B, int *C)
+{
+#pragma omp parallel num_threads(4)
     {
-        for(int i = 0; i < N; i++){
+#pragma omp for schedule(static, 2)
+        for (int i = 0; i < N; i++)
+        {
             C[i] = A[i] + B[i];
             printf("Thread %d calculating C[%d] = A[%d] + B[%d]\n", omp_get_thread_num(), i, i, i);
-        }   
+        }
+
+#pragma omp single
+        {
+            printf("Thread %d executed the single construct \n", omp_get_thread_num());
+        }
+    printf("Thread %d executed this----- \n", omp_get_thread_num());
     }
+    
 }
 
-
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
     int *A, *B, *C;
-    A = malloc(sizeof(int)*N);
-    B = malloc(sizeof(int)*N);
-    C = malloc(sizeof(int)*N);
-    
+    A = malloc(sizeof(int) * N);
+    B = malloc(sizeof(int) * N);
+    C = malloc(sizeof(int) * N);
+
     initVector(A, B, C);
 
     addVector(A, B, C);
--- src/introduction/addVectors/addVectors_sections.c
+++ src/introduction/addVectors/addVectors_sections.c
@@ -4,29 +4,52 @@
 
 #define N 8
 
-void initVector(int *A, int *B, int *C){
-    for(int i = 0; i < N; i++){
+void initVector(int *A, int *B, int *C)
+{
+    for (int i = 0; i < N; i++)
+    {
         A[i] = i;
         B[i] = N - i;
         C[i] = 0;
     }
 }
 
-void addVector(int *A, int *B, int *C){
-    for(int i = 0; i < N; i++){
-        C[i] = A[i] + B[i];
-        printf("Thread %d calculating C[%d] = A[%d] + B[%d]\n", omp_get_thread_num(), i, i, i);
-    }   
+void addVector(int *A, int *B, int *C)
+{
+
+#pragma omp parallel num_threads(4)
+    {
+
+#pragma omp sections
+        {
+#pragma omp section
+            {
+                for (int i = 0; i < N / 2; i++)
+                {
+                    C[i] = A[i] + B[i];
+                    printf("Thread %d calculating C[%d] = A[%d] + B[%d]\n", omp_get_thread_num(), i, i, i);
+                }
+            }
+#pragma omp section
+            {
+                for (int i = N / 2; i < N; i++)
+                {
+                    C[i] = A[i] + B[i];
+                    printf("Thread %d calculating C[%d] = A[%d] + B[%d]\n", omp_get_thread_num(), i, i, i);
+                }
+            }
+        }
+    }
 }
 
-
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
     int *A, *B, *C;
-    A = malloc(sizeof(int)*N);
-    B = malloc(sizeof(int)*N);
-    C = malloc(sizeof(int)*N);
-    
+    A = malloc(sizeof(int) * N);
+    B = malloc(sizeof(int) * N);
+    C = malloc(sizeof(int) * N);
+
     initVector(A, B, C);
 
     addVector(A, B, C);
